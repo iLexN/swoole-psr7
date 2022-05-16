da7ca8b891173536839421990e34ed421a358a4e
iLexN <ilex.job@gmail.co>
2022-05-16 12:19:20 +0800

    reactor config update

--- rector.php
+++ rector.php
@@ -2,26 +2,25 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Core\ValueObject\PhpVersion;
+use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
+return static function (RectorConfig $rectorConfig): void {
 
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::CODING_STYLE);
-    $containerConfigurator->import(SetList::PHP_80);
-    $containerConfigurator->import(SetList::PHP_81);
-    $containerConfigurator->import(SetList::PSR_4);
-    $containerConfigurator->import(SetList::EARLY_RETURN);
-    $containerConfigurator->import(SetList::NAMING);
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::CODING_STYLE,
+        SetList::PHP_80,
+        SetList::PHP_81,
+        SetList::PSR_4,
+        SetList::EARLY_RETURN,
+        SetList::NAMING,
+    ]);
 
-    $parameters->set(Option::SKIP, [
+
+    $rectorConfig->skip([
         Rector\Php80\Rector\FunctionLike\UnionTypesRector::class
     ]);
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_81);
 
 };
