e27b00684fa53278529aeb76d42ad917b8d8ca16
iLexN <iLexN@users.noreply.github.com>
2023-08-15 02:41:29 +0000

    rector config fix

--- rector.php
+++ rector.php
@@ -13,7 +13,7 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::CODING_STYLE,
         SetList::PHP_80,
         SetList::PHP_81,
-        SetList::PSR_4,
+        //SetList::PSR_4,
         SetList::EARLY_RETURN,
         SetList::NAMING,
     ]);
